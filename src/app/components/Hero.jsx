4e8bf3754ea6d7ee16aa9df971e44f0538d179e8
Gary <gkozlowskibusiness@outlook.com>
2023-11-20 19:09:24 -0500

    commit 20 - desktop styling error

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -10,8 +10,9 @@ const Hero = () => {
         <div name="hero" className="grid grid-cols-1 items-center h-screen pb-36 md:pb-96 md:h-max mt-0 
   justify-center  bg-gradient-to-b from-blue-900 via-gray-900 to-blue-900">
            <div className="absolute w-9/12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-           z-10  mb-96 mt-20 md:mb-40 md:mt-96">
-                       <p className="text-xs text-gray-400 md:text-white justify-center mt-2 flex mx-auto text-center p-4 md:p-4">The Revamp Your Ride Platform</p>
+           z-10  mb-96 mt-20 md:mb-10 md:mt-96">
+                       <p className="text-sm md:text-lg text-gray-400 md:text-white justify-center mt-2 flex mx-auto text-center  md:p-4 
+                       font-montserrat md:font-monument-extended">The Platform that Brings You Places</p>
                 <h1 className="text-2xl md:text-5xl justify-center flex text-white p-2">AutoBlockExplorer</h1>
                 </div>
             <div className="cols-span-1 mt-10 pt-10 md:pt-0 md:mt-0"><video     
